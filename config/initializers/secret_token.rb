cf153ac57e87fd6868262e4e602811a01c27527e
Matheus Lima <matheusml90@gmail.com>
2014-04-06 18:54:44 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cartolafc::Application.config.secret_key_base = 'd07a87dd4cd40071a3dc11f81252c34a848faa0e165971c3dacb5ace776bc74bc356fa56905b46ff652ee58ac33608025aafaf029ed7c3f412a3b747653ee0b5'
